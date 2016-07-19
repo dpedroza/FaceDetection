f2ac6e83e70e4717d01cdef4a3cb6b8d02fdb6a6
dpedroza <mv.92@hotmail.com>
2016-07-19 15:45:47 -0300

    With Gallery Pictures

--- app/src/main/java/david/example/com/facedetection/MainActivity.java
+++ app/src/main/java/david/example/com/facedetection/MainActivity.java
@@ -1,5 +1,8 @@
 package david.example.com.facedetection;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
@@ -16,29 +19,43 @@ import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    Bitmap bm = null;
+    ImageView myImageView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myImageView = (ImageView) findViewById(R.id.imgview);
+
+        Button btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Escolher Foto"), 1);
+            }
+        });
+
         Button btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                ImageView myImageView = (ImageView) findViewById(R.id.imgview);
                 BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inMutable=true;
-                Bitmap myBitmap = BitmapFactory.decodeResource(
-                        getApplicationContext().getResources(),
-                        R.drawable.test2,
-                        options);
+                options.inMutable = true;
+                Bitmap myBitmap = bm;
 
                 Paint myRectPaint = new Paint();
                 myRectPaint.setStrokeWidth(5);
@@ -52,14 +69,14 @@ public class MainActivity extends AppCompatActivity {
                 FaceDetector faceDetector = new
                         FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
                         .build();
-                if(!faceDetector.isOperational()){
+                if (!faceDetector.isOperational()) {
                     new AlertDialog.Builder(v.getContext()).setMessage("Could not set up the face detector!").show();
                 }
 
                 Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
                 SparseArray<Face> faces = faceDetector.detect(frame);
 
-                for(int i=0; i<faces.size(); i++) {
+                for (int i = 0; i < faces.size(); i++) {
                     Face thisFace = faces.valueAt(i);
                     float x1 = thisFace.getPosition().x;
                     float y1 = thisFace.getPosition().y;
@@ -67,13 +84,29 @@ public class MainActivity extends AppCompatActivity {
                     float y2 = y1 + thisFace.getHeight();
                     tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
                 }
-                myImageView.setImageDrawable(new BitmapDrawable(getResources(),tempBitmap));
+
+                myImageView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
 
             }
         });
 
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (data != null) {
+            Uri pickedImage = data.getData();
+
+            try {
+                bm = MediaStore.Images.Media.getBitmap(this.getContentResolver(), pickedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            myImageView.setImageBitmap(bm);
+        }
 
     }
 }
